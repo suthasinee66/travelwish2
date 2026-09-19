7ef112a5963094c2192e81987e2cfa7179233c89
suthasinee66 <suthasineeb66@nu.ac.th>
2026-09-20 02:02:30 +0700

    fix: load global styles directly in root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,7 @@ import {
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
 
-import appCss from "../styles.css?url";
+import "../styles.css";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
 function NotFoundComponent() {
@@ -87,10 +87,6 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:site", content: "@Lovable" },
     ],
     links: [
-  {
-    rel: "stylesheet",
-    href: appCss,
-  },
   {
     rel: "preconnect",
     href: "https://fonts.googleapis.com",
